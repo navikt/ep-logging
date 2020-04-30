418530734ad58a553b9afd9f6634d575f6b08b91
moamini <mo.amini@nav.no>
2020-04-30 08:35:23 +0200

    Slår sammen auditlogger fra fagmodul og api-fss

--- src/main/kotlin/no/nav/eessi/pensjon/logging/AuditLogger.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/logging/AuditLogger.kt
@@ -1,7 +1,7 @@
 package no.nav.eessi.pensjon.logging
 
-import no.nav.security.oidc.context.OIDCClaims
 import no.nav.security.oidc.context.OIDCRequestContextHolder
+import no.nav.security.oidc.context.TokenContext
 import no.nav.security.spring.oidc.SpringOIDCRequestContextHolder
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -11,28 +11,46 @@ class AuditLogger(private val oidcRequestContextHolder: OIDCRequestContextHolder
     private val logger = LoggerFactory.getLogger("auditLogger")
 
     // Vi trenger denne no arg konstruktøren for å kunne bruke @Spy med mockito
-    constructor() : this( SpringOIDCRequestContextHolder() )
+    constructor() : this(SpringOIDCRequestContextHolder())
 
-    //brukerident: Z990638 tjenesten: getBucogSedView
     fun log(tjenesteFunctionName: String) {
-        logger.info("brukerident: ${getSubject()} tjenesten: $tjenesteFunctionName")
+        logger.info("brukerident: ${getSubjectfromToken()} tjenesten: $tjenesteFunctionName")
     }
 
-    private fun getSubject(): String {
+    fun log(tjenesteFunctionName: String, aktoerId: String) {
+        logger.info("brukerident: ${getSubjectfromToken()} tjenesten: $tjenesteFunctionName aktoerId: $aktoerId")
+    }
+
+    fun logBorger(tjenesteFunctionName: String, borgerfnr: String) {
+        logger.info("brukerident: ${getSubjectfromToken()} tjenesten: $tjenesteFunctionName fnr: $borgerfnr")
+    }
+
+    fun logBorgerErr(tjenesteFunctionName: String, borgerfnr: String, errorMelding: String) {
+        logger.info("brukerident: ${getSubjectfromToken()} tjenesten: $tjenesteFunctionName fnr: $borgerfnr feilmelding: $errorMelding")
+    }
+
+    private fun getSubjectfromToken() : String {
         return try {
-            getClaims(oidcRequestContextHolder).subject
+            val context = oidcRequestContextHolder.oidcValidationContext
+            val tokenContext = getTokenContext("isso")
+            val issuer = tokenContext.issuer
+            context.getClaims(issuer).subject
         } catch (ex: Exception) {
             logger.error("Brukerident ikke funnet")
             "n/a"
         }
     }
 
-    private fun getClaims(oidcRequestContextHolder: OIDCRequestContextHolder): OIDCClaims {
+    private fun getTokenContext(tokenKey: String): TokenContext {
         val context = oidcRequestContextHolder.oidcValidationContext
-        if(context.issuers.isEmpty())
-            throw RuntimeException("No issuer found in context")
-        val issuer = context.issuers.first()
-        return context.getClaims(issuer)
+        if (context.issuers.isEmpty()) throw RuntimeException("No issuer found in context")
+        val tokenkeys = context.issuers
+        if (tokenkeys.contains(tokenKey)) {
+            return context.getToken(tokenKey)
+        }
+        throw RuntimeException("No issuer found in context")
     }
+
+
 
 }
