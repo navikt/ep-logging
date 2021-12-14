bee2e4c9c7e2bc997a6b9981784da4f51d7358fd
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2021-12-14 15:01:16 +0100

    Bumper versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.0"
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
     `java-library`
     id("net.researchgate.release") version "2.8.1"
     `maven-publish`
     id("com.adarshr.test-logger") version "3.1.0"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.6.0"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.6.10"
     id("com.github.ben-manes.versions") version "0.39.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("org.owasp.dependencycheck") version "6.5.0.1"
@@ -41,7 +41,7 @@ val junitVersion by extra("5.8.+")
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("no.nav.security:token-validation-spring:$oidcTokenSupportVersion")
-    implementation("ch.qos.logback:logback-classic:1.2.7")
+    implementation("ch.qos.logback:logback-classic:1.2.8")
     implementation("org.springframework:spring-web:$springVersion")
     implementation("org.springframework:spring-context:$springVersion")
     compileOnly("javax.servlet:javax.servlet-api:4.0.1")
