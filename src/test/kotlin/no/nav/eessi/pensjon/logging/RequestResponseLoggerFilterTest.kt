1f60cddf37ae037dd0010ddb3797362600c154ff
mariampervez <mariam.pervez@nav.no>
2023-03-02 16:05:12 +0100

    t - Starter på test for RequestResponseLoggerFilter
    Co-authored-by: Kjetil-Jorgensen-Dahl@nav.no

---
+++ src/test/kotlin/no/nav/eessi/pensjon/logging/RequestResponseLoggerFilterTest.kt
@@ -0,0 +1,14 @@
+package no.nav.eessi.pensjon.logging
+
+import org.junit.jupiter.api.Test
+
+class RequestResponseLoggerFilterTest {
+
+
+    @Test
+    fun `Hvis debug er enablet logger vi requesten`(){
+
+//        val actual =
+//        assertEquals("/foo?bar=baz", actual)
+    }
+}
